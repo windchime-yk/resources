eef99309fb3d863f8a0ef2b5879c52ccd5b4a892
windchime-yk <windchimeyk@gmail.com>
2018-10-13 14:43:08 +0900

    modify: 結果報告をコマンド出力からOS通知に変更

--- imgmin.js
+++ imgmin.js
@@ -3,6 +3,7 @@ const pngquant = require('imagemin-pngquant');
 const mozjpeg = require('imagemin-mozjpeg');
 const gifsicle = require('imagemin-gifsicle');
 const svgo = require('imagemin-svgo');
+const notice = require('node-notifier');
 
 imgmin(['src/**/*.{jpg,jpeg,png,gif,svg}'], 'dist', {
   plugins: [
@@ -12,5 +13,5 @@ imgmin(['src/**/*.{jpg,jpeg,png,gif,svg}'], 'dist', {
     svgo()
   ]
 }).then(() => {
-  console.log('画像圧縮が終了しました');
+  notice.notify('画像圧縮が終了しました')
 });
