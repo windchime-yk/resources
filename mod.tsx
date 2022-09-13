6a553275bb8baa667b380c56a05b5e73268de263
WhyK <windchimeyk@gmail.com>
2022-09-14 00:32:55 +0900

    fix: replace isExistFileSync

--- mod.tsx
+++ mod.tsx
@@ -4,7 +4,7 @@ import {
   serve,
 } from "https://deno.land/std@0.117.0/http/server.ts";
 import { h, html } from "https://deno.land/x/htm@0.0.10/mod.tsx";
-import { isExistFileSync } from "https://pax.deno.dev/windchime-yk/deno-util@v1.6.0/file.ts";
+import { isExistFile } from "https://pax.deno.dev/windchime-yk/deno-util@v1.6.0/file.ts";
 import { contentType } from "https://deno.land/std@0.152.0/media_types/mod.ts";
 
 const handler: Handler = async (req) => {
@@ -28,7 +28,7 @@ const handler: Handler = async (req) => {
   const typeName = router.exec(req.url)?.pathname.groups.type;
   const fileName = `./src/${pathname}`;
 
-  if (isExistFileSync(fileName)) {
+  if (await isExistFile(fileName)) {
     if (typeName === "js") {
       const jsFile = await Deno.readFile(fileName);
       return new Response(jsFile, {
